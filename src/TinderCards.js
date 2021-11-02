aedcfa7362cd27cb8513f332bc634fd47dff0fc6
Andrew Smith <ajs3993@gmail.com>
2021-11-02 17:53:20 -0500

    use state

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './TinderCards.css'
 
 function TinderCards() {
     
-    
+    const [people, setPeople] = useState([
+        {
+            name: 'Elon',
+            url: "https://compote.slate.com/images/d30a3bea-f2dc-4b95-b889-16c575ff568c.jpeg?width=780&height=520&rect=5113x3409&offset=0x0"
+        }
+    ]);
+
     return (
         <div className='tinderCards'>
             
